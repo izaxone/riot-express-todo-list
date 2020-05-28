f37123aa79190d1b51d16d24fc601736eae120a8
Kevin Isham <5745349-izaxone@users.noreply.gitlab.com>
2020-05-28 16:21:39 -0600

    Updated IP address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.234.106.201',
+      host: '52.91.135.134',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
