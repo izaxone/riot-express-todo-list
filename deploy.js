5492afbdeccf683391bae0b0e3334b62856ce8f3
Kevin Isham <5745349-izaxone@users.noreply.gitlab.com>
2020-05-28 16:27:29 -0600

    Changed deploy directory to riot-express-todo-list

--- deploy.js
+++ deploy.js
@@ -23,7 +23,7 @@ function installPM2() {
 function transferProjectToRemote(failed, successful) {
   return ssh.putDirectory(
     '../riot-express-todo-list',
-    '/home/ubuntu/riot-express-todo-list-temp',
+    '/home/ubuntu/riot-express-todo-list',
     {
       recursive: true,
       concurrency: 1,
@@ -49,7 +49,7 @@ function transferProjectToRemote(failed, successful) {
 // creates a temporary folder on the remote server
 function createRemoteTempFolder() {
   return ssh.execCommand(
-    'rm -rf riot-express-todo-list-temp && mkdir riot-express-todo-list-temp', {
+    'rm -rf riot-express-todo-list && mkdir riot-express-todo-list', {
       cwd: '/home/ubuntu'
   });
 }
@@ -65,7 +65,7 @@ function stopRemoteServices() {
 // updates the project source on the server
 function updateRemoteApp() {
   return ssh.execCommand(
-    'mkdir riot-express-todo-list && cp -r riot-express-todo-list-temp/* riot-express-todo-list/ && rm -rf riot-express-todo-list-temp', {
+    'mkdir riot-express-todo-list && cp -r riot-express-todo-list/* riot-express-todo-list/ && rm -rf riot-express-todo-list', {
       cwd: '/home/ubuntu'
   });
 }
@@ -95,7 +95,7 @@ function sshConnect() {
       return installPM2();
     })
     .then(function() {
-      console.log('Creating `riot-express-todo-list-temp` folder.');
+      console.log('Creating `riot-express-todo-list` folder.');
       return createRemoteTempFolder();
     })
     .then(function(result) {
